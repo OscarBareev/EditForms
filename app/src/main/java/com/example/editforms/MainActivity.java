57008f9f8e2e4c4086c40dd8259cc74dc5ec5a4e
Oscar Bareev <oscar.bareev@gmail.com>
2020-05-01 10:49:20 +0300

    Fix MainActivity

--- app/src/main/java/com/example/editforms/MainActivity.java
+++ app/src/main/java/com/example/editforms/MainActivity.java
@@ -1,57 +1,58 @@
 package com.example.editforms;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText email;
+    private TextView textField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText name = (EditText) findViewById(R.id.nameField);
-        final EditText email = (EditText) findViewById(R.id.mailField);
-        final TextView textField = (TextView) findViewById(R.id.putText);
-        final Button okButton = (Button) findViewById(R.id.okButton);
-        final Button cleanButton = (Button) findViewById(R.id.cleanButton);
+        name = findViewById(R.id.nameField);
+        email = findViewById(R.id.mailField);
+        textField = findViewById(R.id.putText);
 
-        okButton(name, email, textField, okButton);
-        cleanButton(name, email, textField, cleanButton);
-    }
-
-
-    private void okButton(final EditText name, final EditText email, final TextView textField, final Button okButton) {
-
+        Button okButton = findViewById(R.id.okButton);
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userName = name.getText().toString();
                 String userMail = email.getText().toString();
 
-                if (userName.trim().equals("") || userMail.trim().equals("")) {
-                    textField.setText("Введите данные!");
+                if (userName.trim().isEmpty() || userMail.trim().isEmpty()) {
+                    textField.setText(R.string.addData);
                 } else {
-                    textField.setText("Подписка на рассылку успешно оформлена для пользователя " +
-                            userName + " на электронный адрес " + userMail);
+                    textField.setText(getString(R.string.subscribe) +
+                            userName + getString(R.string.yourMail) + userMail);
                 }
             }
         });
-    }
 
-    private void cleanButton(final EditText name, final EditText email, final TextView textField, final Button cleanButton) {
+        Button cleanButton = findViewById(R.id.cleanButton);
         cleanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                email.setText("");
                 textField.setText("");
                 name.setText("");
-                email.setText("");
+
             }
         });
+
+
     }
+
+
 }
